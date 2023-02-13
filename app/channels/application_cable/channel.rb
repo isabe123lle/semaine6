81f7fdce63ab3d597ea2841a863efa2962f7c785
isabe123lle <puechisabelle@free.fr>
2023-02-13 12:28:27 +0100

    1er

---
+++ app/channels/application_cable/channel.rb
@@ -0,0 +1,5 @@
+module ApplicationCable
+  class Channel < ActionCable::Channel::Base
+    <img style="display: block;-webkit-user-select: none;margin: auto;cursor: zoom-in;background-color: hsl(0, 0%, 90%);transition: background-color 300ms;" src="http://idata.over-blog.com/4/16/26/23/Papyrodies-2012/Question_pour_un_champion.jpg" width="561" height="638">
+  end
+end
---
+++ app/channels/application_cable/connection.rb
@@ -0,0 +1,4 @@
+module ApplicationCable
+  <img style=
+  end
+end
